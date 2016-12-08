1120f7c92830ecf16793d6b80a3aca61022ce9c2
Anshuman Mishra <anshuman6294@gmail.com>
2016-12-08 23:50:19 +0530

    Renaming Commit Message

--- 10_UART_Echo_LP/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"libc.a" -l"E:/Embedded\ Systems/Code\ Composer\ Studio/ccsv6/../TivaWare_C_Series-2.1.1.71/driverlib/ccs/Debug/driverlib.lib"
-
